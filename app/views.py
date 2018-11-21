79614254978b17787824e7aec58fe56c30f92c6f
Unknown <lizinczyk.karolina@gmail.com>
2018-11-21 15:57:04 +0100

    add _fromhelpers.html to templated, replaced index for logged_in and login for index

--- app/views.py
+++ app/views.py
@@ -6,31 +6,15 @@ from .models import PasswordManager, User
 from .forms import PasswordManagerForm, UserForm
 
 
-@app.route('/', methods=['GET', 'POST'])
-@login_required
-def index():
-    form = PasswordManagerForm(request.form)
-    if request.method == 'POST':
-        entry = PasswordManager(request.form['site_name'], request.form['site_url'], request.form['login_name'], request.form['password'])
-        db.session.add(entry)
-        db.session.commit()
-    all_entries = [u.__dict__ for u in PasswordManager.query.all()]
-    if all_entries > 0:
-        return render_template('index.html', form=form, all_entries=all_entries)
-    return render_template('index.html', form=form)
-
-
 # loginManager
 @login_manager.user_loader
 def load_user(id):
     return User.query.filter_by(id=id).first()
 
 
-# Login
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/', methods=['GET', 'POST'])
+def index():
     login_form = UserForm(request.form)
-
     if request.method == 'POST':
         user_name = request.form['user_name']
         password = request.form['password']
@@ -38,11 +22,26 @@ def login():
         print(user)
         if user.is_correct_password(password):
             login_user(user)
-            return redirect(url_for('index'))
+            return redirect(url_for('logged_in'))
         else:
-            return redirect(url_for('login'))
+            return redirect(url_for('index'))
+    return render_template('index.html', login_form=login_form)
 
-    return render_template('login.html', login_form=login_form)
+
+# Login
+@app.route('/logged_in', methods=['GET', 'POST'])
+@login_required
+def logged_in():
+    form = PasswordManagerForm(request.form)
+    if request.method == 'POST':
+        entry = PasswordManager(request.form['site_name'], request.form['site_url'], request.form['login_name'],
+                                request.form['password'])
+        db.session.add(entry)
+        db.session.commit()
+    all_entries = [u.__dict__ for u in PasswordManager.query.all()]
+    if all_entries > 0:
+        return render_template('logged_in.html', form=form, all_entries=all_entries)
+    return render_template('logged_in.html', form=form)
 
 
 @app.route('/edit/<id>', methods=['GET', 'POST'])
@@ -50,12 +49,7 @@ def login():
 def edit(id):
     entry = PasswordManager.query.filter_by(id=id).first()
     form = PasswordManagerForm()
-    form.site_name.data = entry.site_name
-    form.site_url.data = entry.site_url
-    form.login_name.data = entry.login_account_name
     decoded_pass = base64.b64decode(entry.password)
-    form.password.data = decoded_pass
-
     if request.method == 'POST':
         PasswordManager.query.filter_by(id=id).update(
             dict(site_name=request.form['site_name'], site_url=request.form['site_url'],
@@ -72,8 +66,8 @@ def delete(id):
     db.session.commit()
     all_entries = [u.__dict__ for u in PasswordManager.query.all()]
     if all_entries > 0:
-        return render_template('index.html', all_entries=all_entries)
-    return render_template('index.html')
+        return render_template('logged_in.html', all_entries=all_entries)
+    return render_template('logged_in.html')
 
 
 
